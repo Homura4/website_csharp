f55ec9a1266d403f4530ee7d3d5f77b6c66fcdeb
Homura <1573404187@qq.com>
2018-10-19 18:08:12 +0800

    基础页面登陆与模板页

--- website/App_Start/BundleConfig.cs
+++ website/App_Start/BundleConfig.cs
@@ -8,25 +8,45 @@ namespace website
         // 有关捆绑的详细信息，请访问 https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            #region 母版页
+            // js
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                        "~/Content/Module/jquery/dist/jquery.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                        "~/Content/Module/bootstrap/dist/js/bootstrap.min.js"));
 
-            // 框架所需
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/font-awesome.min.css",
-                      "~/Content/nprogress.css",
-                      "~/Content/custom.min.css"));
             bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
-                      "~/Scripts/fastclick.js",
-                      "~/Scripts/nprogress.js",
-                      "~/Scripts/custom.min.js"));
+                        "~/Content/Module/fastclick/lib/fastclick.js"));
+
+            bundles.Add(new StyleBundle("~/bundles/nprogress").Include(
+                        "~/Content/Module/nprogress/nprogress.js"));
+
+            // css
+            bundles.Add(new StyleBundle("~/Content/bootstrap").Include(
+                        "~/Content/Module/bootstrap/dist/css/bootstrap.css"));
+
+            bundles.Add(new StyleBundle("~/Content/font-awesome").Include(
+                        "~/Content/Module/font-awesome/css/font-awesome.min.css"));
+
+            bundles.Add(new StyleBundle("~/Content/nprogress").Include(
+                        "~/Content/Module/nprogress/nprogress.css"));
+
+            //自定义公共css与js
+            bundles.Add(new StyleBundle("~/bundles/custom").Include(
+                        "~/Content/Module/build/js/custom.min.js"));
+
+            bundles.Add(new StyleBundle("~/Content/custom").Include(
+                        "~/Content/Module/build/css/custom.min.css"));
+
+            #endregion 母版页
+
+            #region 模块
+
+            bundles.Add(new StyleBundle("~/Content/animate").Include(
+                        "~/Content/Module/animate.css/animate.min.css"));
+
+            #endregion
         }
     }
 }
